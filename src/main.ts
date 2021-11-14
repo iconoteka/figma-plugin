38fd3f247a73e9373020f53a91a2c140f8581a1a
Aleksei Chernov <kot.lex@gmail.com>
2021-11-15 01:07:33 +0300

    Add inline

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,38 @@ import { on, once, showUI } from '@create-figma-plugin/utilities'
 import { CloseHandler, CreateRectanglesHandler } from './types'
 
 export default function () {
-  once<CreateRectanglesHandler>('CREATE_RECTANGLES', function (count: number) {
-    const nodes: Array<SceneNode> = []
-    for (let i = 0; i < count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [
-        {
-          type: 'SOLID',
-          color: { r: 1, g: 0.5, b: 0 }
-        }
-      ]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
-    }
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
-    figma.closePlugin()
-  })
+  let resolveIcons = (icons: any) => {};
+
+  const getIconsPromise  = (query = '') => {
+    return new Promise<any>((resolve) => {
+      
+      resolveIcons = resolve;
+    });
+  }
+  let withUI = false;
+
+  let iconsPromise = getIconsPromise();
+
+  // Create an invisible iframe UI to use network API's
+  figma.showUI(
+    `<script>
+    window.onmessage = async (event) => {
+      const query = event.data.pluginMessage.query;
+      const style = event.data.pluginMessage.style;
+      const weight = event.data.pluginMessage.weight;
+      const res = await fetch("https://staging.iconoteka.com:8080/icons/style/"+style+"/weight/"+weight+"?" + new URLSearchParams({
+        query,
+    }));
+      const json = await res.json();
+      window.parent.postMessage({ pluginMessage: ['BACKEND_RESPONSE', json] }, "*");
+    };
+    </script><h1>Icons</h1>`,
+    { visible: false }
+  );
+
+  on('BACKEND_RESPONSE', (json) => {
+    resolveIcons(json);
+  });
 
   on('ADD_ICON', (icon) => {
     const iconNode = figma.createNodeFromSvg(icon.svg);
@@ -29,11 +43,52 @@ export default function () {
     iconNode.y = figma.viewport.center.y;
   });
 
-  once<CloseHandler>('CLOSE', function () {
-    figma.closePlugin()
-  })
-  showUI({
-    width: 325,
-    height: 500
-  })
+
+  figma.parameters.on(
+    'input',
+    async ({ key, query, result, parameters }: ParameterInputEvent) => {
+  
+      switch(key) {
+        case "style":
+          result.setSuggestions(['Fill', 'Stroke'])
+        break;
+        case "weight":
+          result.setSuggestions(['Light', 'Regular', 'Medium', 'Bold'])
+        break;
+        default:
+          if (query.length < 3) {
+            result.setLoadingMessage('Provide icon name');
+          } else {
+            result.setLoadingMessage('Loading icons');
+            figma.ui.postMessage({ query, style: parameters.style, weight: parameters.weight });
+            const icons = await iconsPromise;
+  
+            result.setSuggestions(
+              icons.map((icon: any) => ({ name: icon.name, data: { name: icon.name, svg: icon.svg }, icon: icon.svg}))
+            );
+  
+            iconsPromise = getIconsPromise(query);
+          }
+      }
+    }
+  );
+  
+  figma.on('run', ({ command, parameters }: RunEvent) => {
+  
+    if (parameters?.icon) {
+      const icon = figma.createNodeFromSvg(parameters?.icon.svg);
+      icon.name = parameters?.icon.name;
+      icon.x = figma.viewport.center.x;
+      icon.y = figma.viewport.center.y;
+  
+      figma.closePlugin(`${parameters?.icon.name} icon added`);
+    } else {
+      showUI({
+        width: 325,
+        height: 500
+      });
+    }
+  });
+  
+
 }
