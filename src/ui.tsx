36edf193966256d4567eff61547b948ebd65919a
Aleksei Chernov <aleksei.chernov@riverside.fm>
2023-07-29 20:42:43 +0300

    Format code

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,6 +1,4 @@
 import {
-  Button,
-  Columns,
   Container,
   Divider,
   LoadingIndicator,
@@ -9,55 +7,53 @@ import {
   SearchTextbox,
   SegmentedControl,
   Text,
-  TextboxNumeric,
-  VerticalSpace
-} from '@create-figma-plugin/ui';
-import styles from './ui.css';
+  VerticalSpace,
+} from "@create-figma-plugin/ui";
+import styles from "./ui.css";
 import useObserver from "preact-intersection-observer";
-import { version }from "../package.json";
-const backendUrl = 'https://staging.iconoteka.com:8080';
+import { version } from "../package.json";
+const backendUrl = "https://staging.iconoteka.com:8080";
 
-import { emit, EventHandler } from '@create-figma-plugin/utilities'
-import { h } from 'preact'
-import { useCallback, useState, useEffect } from 'preact/hooks'
+import { emit } from "@create-figma-plugin/utilities";
+import { h } from "preact";
+import { useState, useEffect } from "preact/hooks";
 
-import { CloseHandler, CreateRectanglesHandler } from './types'
 
 const Footer = () => {
   return (
-  <div className={styles.footer}>
-    <div className={styles.footer__about}>
-      <a href="https://readymag.com/turbaba/1296248/" target="_blank">About</a>
+    <div className={styles.footer}>
+      <div className={styles.footer__about}>
+        <a href="https://readymag.com/turbaba/1296248/" target="_blank">
+          About
+        </a>
+      </div>
+      <div className={styles.footer__support}>
+        <a href="https://www.patreon.com/iconoteka" target="_blank">
+          Support us on Patreon
+        </a>
+      </div>
+      <div className={styles.footer__version}>v{version}</div>
     </div>
-    <div className={styles.footer__support}>
-      <a href="https://www.patreon.com/iconoteka" target="_blank">Support us on Patreon</a>
-    </div>
-    <div className={styles.footer__version}>
-      v{version}
-    </div>
- </div>
- )
-}
+  );
+};
 function Plugin() {
   const [icons, setIcons] = useState<null | any[]>(null);
   const [searchParams, setSearchParams] = useState({
-    weight: 'bold',
-    style: 'stroke',
-    query: '',
+    weight: "bold",
+    style: "stroke",
+    query: "",
   });
 
   useEffect(() => {
     (async () => {
-          
-      const result:any = await fetch(
+      const result: any = await fetch(
         `${backendUrl}/icons/style/${searchParams.style}/weight/${searchParams.weight}?query=${searchParams.query}&useGroups=true`
       );
       const icons = await result.json();
 
       setIcons(icons);
     })();
-
-}, [searchParams.weight, searchParams.style, searchParams.query]); 
+  }, [searchParams.weight, searchParams.style, searchParams.query]);
 
   const handleWeightChange = (event: any) => {
     const value = event.target.value;
@@ -73,7 +69,7 @@ function Plugin() {
       ...searchParams,
       style: value,
     });
-  } 
+  };
 
   const handleQueryChange = (event: any) => {
     const value = event.target.value;
@@ -81,67 +77,90 @@ function Plugin() {
       ...searchParams,
       query: value,
     });
-  }
+  };
 
   return (
-    <div style={{overflow: 'hidden'}}>
-    <SearchTextbox
-      value={searchParams.query}
-      placeholder="Search"
-      onInput={handleQueryChange}
-    />
-    <Container>
-    <div class={styles['style-selector']}>
-      <SegmentedControl onChange={handleWeightChange} options={[{children: 'Bold', value: 'bold'}, {children: 'Medium', value: 'medium'}, {children: 'Regular', value: 'regular'}, {children: 'Light', value: 'light'} ]} value={searchParams.weight} />
+    <div style={{ overflow: "hidden" }}>
+      <SearchTextbox
+        value={searchParams.query}
+        placeholder="Search"
+        onInput={handleQueryChange}
+      />
+      <Container>
+        <div class={styles["style-selector"]}>
+          <SegmentedControl
+            onChange={handleWeightChange}
+            options={[
+              { children: "Bold", value: "bold" },
+              { children: "Medium", value: "medium" },
+              { children: "Regular", value: "regular" },
+              { children: "Light", value: "light" },
+            ]}
+            value={searchParams.weight}
+          />
+        </div>
+        <VerticalSpace space="extraSmall" />
+        <div class={styles["style-selector"]}>
+          <SegmentedControl
+            onChange={handleStyleChange}
+            options={[
+              { children: "Stroke", value: "stroke" },
+              { children: "Fill", value: "fill" },
+            ]}
+            value={searchParams.style}
+          />
+        </div>
+      </Container>
+      <VerticalSpace space="small" />
+      <Divider />
+      <Container
+        style={{
+          height: "338px",
+          overflowY: "auto",
+          paddingBottom: "20px",
+          boxSizing: "border-box",
+        }}
+      >
+        {icons === null ? (
+          <MiddleAlign>
+            <LoadingIndicator />
+          </MiddleAlign>
+        ) : (
+          icons.map((group: any) => <Group group={group} />)
+        )}
+      </Container>
+      <VerticalSpace space="small" />
+      <Divider />
+      <VerticalSpace space="small" />
+      <Footer />
     </div>
-    <VerticalSpace space="extraSmall" />
-    <div class={styles['style-selector']}>
-      <SegmentedControl onChange={handleStyleChange} options={[{children: 'Stroke', value: 'stroke'}, {children: 'Fill', value: 'fill'}, ]} value={searchParams.style} />
-    </div>
-    </Container>
-    <VerticalSpace space="small" />
-    <Divider />
-    <Container style={{height: '338px', overflowY: 'auto', paddingBottom: '20px', boxSizing: 'border-box'}}>
-     { icons === null 
-        ?  <MiddleAlign><LoadingIndicator /></MiddleAlign>
-        : icons.map((group: any) => (
-       <Group group={group} />
-     ))}
-
-    </Container>
-    <VerticalSpace space="small" />
-    <Divider />
-    <VerticalSpace space="small" />
-    <Footer />
-    </div>
-  )
+  );
 }
 
 function Group(props: any) {
   const { group, ...otherProps } = props;
-  const [sectionRef, inView] = useObserver({threshold: 0.01});
-  console.log(group.name, 'inView', inView);
+  const [sectionRef, inView] = useObserver({ threshold: 0.01 });
+  console.log(group.name, "inView", inView);
 
- return ( 
- <div {...otherProps} ref={sectionRef}>
-    <VerticalSpace space="small" />
-    <Text muted>{group.name}</Text>
-    <div class={styles.iconsContainer}>
-      {group.items.map((icon:any) => (
-        <Icon icon={icon} isVisible={inView} />
-      ))}
+  return (
+    <div {...otherProps} ref={sectionRef}>
+      <VerticalSpace space="small" />
+      <Text muted>{group.name}</Text>
+      <div class={styles.iconsContainer}>
+        {group.items.map((icon: any) => (
+          <Icon icon={icon} isVisible={inView} />
+        ))}
+      </div>
     </div>
-  </div>
-);
-
+  );
 }
 type ImageProps = {
   src: string;
   onClick: (event: any) => void;
-}
+};
 function Image(props: ImageProps) {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [placeholderClass, setPlaceholderClass] = useState('');
+  const [placeholderClass, setPlaceholderClass] = useState("");
 
   useEffect(() => {
     let img = new window.Image();
@@ -151,23 +170,28 @@ function Image(props: ImageProps) {
 
   const onImageLoad = () => {
     setIsLoaded(true);
-  }
+  };
 
   const style = {
-    opacity: isLoaded ? 1 : 0
-  }
+    opacity: isLoaded ? 1 : 0,
+  };
 
   const divStyle = {
-    width: '24px',
-    height: '24px',
+    width: "24px",
+    height: "24px",
     background: `url(${props.src}) no-repeat`,
-  }
+  };
   return (
     <div>
       {/* <div style={divStyle}></div> */}
-      <img src={props.src} class={styles.iconImage} style={style} onClick={props.onClick}/>
+      <img
+        src={props.src}
+        class={styles.iconImage}
+        style={style}
+        onClick={props.onClick}
+      />
     </div>
-  )
+  );
 }
 function Icon(props: any) {
   const { icon, isVisible } = props;
@@ -177,18 +201,17 @@ function Icon(props: any) {
     (async () => {
       const svgRequest = await fetch(url);
       const svg = await svgRequest.text();
-      emit('ADD_ICON', {name: icon.name, svg});
+      emit("ADD_ICON", { name: icon.name, svg });
     })();
   };
 
- return (
-  <div class={styles.iconContainer}>
-    <div className={`${styles.iconPlaceholder}`}></div>
+  return (
+    <div class={styles.iconContainer}>
+      <div className={`${styles.iconPlaceholder}`}></div>
 
-    {isVisible && <Image src={url} onClick={handleClick}/> }
-
-  </div>
- )
+      {isVisible && <Image src={url} onClick={handleClick} />}
+    </div>
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
